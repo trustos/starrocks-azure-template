ecb9466c9792fee15ff94e29b775eb6be4ca50ca
Yavor Tenev <trustos@gmail.com>
2023-01-17 10:47:16 +0200

    Fixes.

--- install.sh
+++ install.sh
@@ -5,24 +5,26 @@ StarRocks_version=2.5.0-rc03
 StarRocks_home=~/data/deploy
 Java_version=java-1.8.0-openjdk-devel.x86_64
 StarRocks_url=https://releases.starrocks.io/starrocks/StarRocks-${StarRocks_version}.tar.gz
-JAVA_INSTALL_DIR=/usr/lib/jvm/$(rpm -aq | grep java-1.8.0-openjdk-1.8.0)
-echo $JAVA_INSTALL_DIR
-export JAVA_HOME=$JAVA_INSTALL_DIR
-
-# Add JAVA_HOME to env variables
-if [! $(cat /etc/environment | grep $JAVA_HOME)]; then
-	echo JAVA_HOME=$JAVA_HOME | sudo tee --append /etc/environment
-fi
 
 # Install StarRocks.
 yum -y install wget
-sudo mkdir -p $StarRocks_home -m 777
+mkdir -p $StarRocks_home -m 777
 wget -SO $StarRocks_home/StarRocks-${StarRocks_version}.tar.gz  $StarRocks_url
 cd $StarRocks_home && tar zxf StarRocks-${StarRocks_version}.tar.gz
 
 # Install Java JDK.
 yum -y install ${Java_version}
 rpm -ql ${Java_version} | grep bin$
+
+
+JAVA_INSTALL_DIR=/usr/lib/jvm/$(rpm -aq | grep java-1.8.0-openjdk-1.8.0)
+echo $JAVA_INSTALL_DIR
+export JAVA_HOME=$JAVA_INSTALL_DIR
+
+# Add JAVA_HOME to env variables
+if ! [ $(cat ~/.bash_profile | grep $JAVA_HOME) ]; then
+	echo JAVA_HOME=$JAVA_HOME | tee --append /etc/environment
+fi
 
 # Create directories for FE meta and BE storage in StarRocks.
 mkdir -p $StarRocks_home/StarRocks-${StarRocks_version}/fe/meta
@@ -36,9 +38,8 @@ cd $StarRocks_home/StarRocks-${StarRocks_version}/fe/bin/
 ./start_fe.sh --daemon
 
 # Add FE to start on boot
-# crontab -l 2>/dev/null; echo "0 4 * * * myscript")| crontab -
-# if [! $(cat /etc/crontab | grep 'start_fe.sh')]; then
-	# echo @reboot $StarRocks_home/StarRocks-${StarRocks_version}/fe/bin/start_fe.sh --daemon | sudo tee --append /etc/crontab
+if ! [ "$(crontab -l 2>/dev/null | grep "/fe/bin/start_fe.sh")" ]; then
+	echo @reboot $StarRocks_home/StarRocks-${StarRocks_version}/fe/bin/start_fe.sh --daemon | crontab -
 fi
 
 # Start BE.
@@ -46,11 +47,9 @@ cd $StarRocks_home/StarRocks-${StarRocks_version}/be/bin/
 ./start_be.sh --daemon
 
 # Add BE to start on boot
-
-# crontab -l 2>/dev/null; echo "0 4 * * * myscript")| crontab -
-# if [! $(cat /etc/crontab | grep 'start_be.sh')]; then
-# 	echo @reboot $StarRocks_home/StarRocks-${StarRocks_version}/be/bin/start_be.sh --daemon | sudo tee --append /etc/crontab
-# fi
+if ! [ "$(crontab -l 2>/dev/null | grep "/be/bin/start_be.sh")" ]; then
+	echo @reboot $StarRocks_home/StarRocks-${StarRocks_version}/be/bin/start_be.sh --daemon | crontab -
+fi
 
 # Sleep until the cluster starts.
 sleep 30;
